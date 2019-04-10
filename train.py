1df8810f09e6610c5a9397334af71c79a678e294
Li Wei <liwei1@sensetime.com>
2019-04-11 01:10:15 +0800

    add cutout mixup

--- train.py
+++ train.py
@@ -37,12 +37,19 @@ def train(train_loader, net, criterion, optimizer, epoch, device):
     for batch_index, (inputs, targets) in enumerate(train_loader):
         # move tensor to GPU
         inputs, targets = inputs.to(device), targets.to(device)
+        if config.mixup:
+            inputs, targets_a, targets_b, lam = mixup_data(inputs, targets,
+                                                           config.mixup_alpha, device)
+
+            outputs = net(inputs)
+            loss = mixup_criterion(
+                criterion, outputs, targets_a, targets_b, lam)
+        else:
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
         # zero the gradient buffers
         optimizer.zero_grad()
-        # forward
-        outputs = net(inputs)
-        # cal the loss
-        loss = criterion(outputs, targets)
         # backward
         loss.backward()
         # update weight
@@ -52,7 +59,11 @@ def train(train_loader, net, criterion, optimizer, epoch, device):
         train_loss += loss.item()
         _, predicted = outputs.max(1)
         total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+        if config.mixup:
+            correct += (lam * predicted.eq(targets_a).sum().item()
+                        + (1 - lam) * predicted.eq(targets_b).sum().item())
+        else:
+            correct += predicted.eq(targets).sum().item()
 
         if (batch_index + 1) % 100 == 0:
             logger.info("   == step: [{:3}/{}], train loss: {:.3f} | train acc: {:6.3f}% | lr: {:.6f}".format(
@@ -125,9 +136,11 @@ def main():
         config = yaml.load(f)
     # convert to dict
     config = EasyDict(config)
+    logger.info(config)
 
     # define netowrk
     net = get_model(config)
+    logger.info(" == total parameters: " + str(count_parameters(net)))
 
     # CPU or GPU
     device = 'cuda' if config.use_gpu else 'cpu'
@@ -163,7 +176,7 @@ def main():
     train_loader, test_loader = get_data_loader(
         transform_train, transform_test, config)
 
-    logger.info("\n=======  Training  =======\n")
+    logger.info("            =======  Training  =======\n")
     for epoch in range(last_epoch + 1, config.epochs):
         lr = adjust_learning_rate(optimizer, epoch, config)
         writer.add_scalar('learning_rate', lr, epoch)
